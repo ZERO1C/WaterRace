31ba0eea5f20a0118931ab2c7089421fcd673a18
ZERO1C <taras.savinsy@gmail.com>
2023-10-16 22:38:52 +0300

    Feel

--- WaterRace/Assets/Code/MovePlayer.cs
+++ WaterRace/Assets/Code/MovePlayer.cs
@@ -8,9 +8,11 @@ public class MovePlayer : MonoBehaviour
     public DynamicJoystick DynamicJoystick;
     public Transform TransformJet;
     public Rigidbody Rb;
+    public GameObject effect;
 
     private float _oldHorizontal;
     private float _newHorizontal;
+    private float waterLineNow;
     private void Awake()
     {
         Rb = GetComponent<Rigidbody>();
@@ -38,32 +40,39 @@ public class MovePlayer : MonoBehaviour
 
             newDifferent = _oldHorizontal - _newHorizontal;
             oldDifferent = Mathf.Lerp(oldDifferent, newDifferent, 4 * Time.deltaTime);
-            Vector3 rotationJet = new Vector3(Mathf.Clamp(-20f * vectorMove.x, -10f, 0f), 90f + _oldHorizontal * 80f, oldDifferent * 40f);
+            Vector3 rotationJet = new Vector3(Mathf.Clamp(-50f * vectorMove.x * Mathf.Clamp( waterLineNow,0,1), -30f, 0f), 90f + _oldHorizontal * 80f, oldDifferent * 40f);
             Debug.Log(rotationJet);
             TransformJet.rotation = Quaternion.Euler(rotationJet);
             yield return new WaitForFixedUpdate();
         }
     }
+
+
     IEnumerator WaterLine()
     {
-        float waterLineNow = -0.02f;
+        float waterLineStep = 0.01f;
+
         while (true)
         {
-            while (waterLineNow < -0.09f)
+            while (waterLineNow > -0.2f)
             {
-                waterLineNow = Mathf.Lerp(waterLineNow, -0.1f, 1 * Time.deltaTime);
+                waterLineNow -= waterLineStep*5f;
+                yield return new WaitForFixedUpdate();
+                BuoyantObject.waterLevelOffset = waterLineNow;
+
+
+            }
+            effect.SetActive(true);
+            yield return new WaitForFixedUpdate();
+
+            while (waterLineNow < 0.4f)
+            {
+                waterLineNow += waterLineStep*1.5f;
                 BuoyantObject.waterLevelOffset = waterLineNow;
                 yield return new WaitForFixedUpdate();
 
             }
-
-            while (waterLineNow > -0.01)
-            {
-                waterLineNow = Mathf.Lerp(waterLineNow, 0, 1 * Time.deltaTime);
-                BuoyantObject.waterLevelOffset = waterLineNow;
-                yield return new WaitForFixedUpdate();
-
-            }
+            yield return new WaitForSeconds(Random.Range(0f, 10f) / 10f);
 
         }
     }
