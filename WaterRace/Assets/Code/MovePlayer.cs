077e1853e819e9df62a40fff8935fc2d1f3cc23c
ZERO1C <taras.savinsy@gmail.com>
2023-10-17 00:59:34 +0300

    Day 1

--- WaterRace/Assets/Code/MovePlayer.cs
+++ WaterRace/Assets/Code/MovePlayer.cs
@@ -8,40 +8,44 @@ public class MovePlayer : MonoBehaviour
     public DynamicJoystick DynamicJoystick;
     public Transform TransformJet;
     public Rigidbody Rb;
-    public GameObject effect;
+    public GameObject Effect;
+    public GameObject RightEffect;
+    public GameObject LeftEffect;
 
     private float _oldHorizontal;
     private float _newHorizontal;
     private float waterLineNow;
+    private float waterLineRot;
+
+    private bool _jetDownMove = false;
+    private float newDifferent;
     private void Awake()
     {
         Rb = GetComponent<Rigidbody>();
         StartCoroutine(MoveJet());
         StartCoroutine(WaterLine());
     }
-    private void FixedUpdate()
-    {
-
-
-    }
     private IEnumerator MoveJet()
     {
         float oldDifferent = 0;
-        float newDifferent = 0;
         while (true)
         {
             _newHorizontal = DynamicJoystick.Horizontal;
             if (_newHorizontal == 0f) _oldHorizontal = Mathf.Lerp(_oldHorizontal, _newHorizontal, 2f * Time.deltaTime);
             else _oldHorizontal = Mathf.Lerp(_oldHorizontal, _newHorizontal, 3f * Time.deltaTime);
-            Debug.Log((_oldHorizontal - _newHorizontal));
 
             Vector3 vectorMove = new Vector3(1f - Mathf.Abs(_oldHorizontal) / 1.2f, 0f, -_oldHorizontal/1.3f);
             Rb.velocity = vectorMove * 15f;
 
             newDifferent = _oldHorizontal - _newHorizontal;
+            if (Mathf.Abs(newDifferent) > 0.5f) _jetDownMove = true;
+            if (newDifferent > 0.5f) RightEffect.SetActive(true);
+            else if (newDifferent < -0.5f) LeftEffect.SetActive(true);
+
+
             oldDifferent = Mathf.Lerp(oldDifferent, newDifferent, 4 * Time.deltaTime);
-            Vector3 rotationJet = new Vector3(Mathf.Clamp(-50f * vectorMove.x * Mathf.Clamp( waterLineNow,0,1), -30f, 0f), 90f + _oldHorizontal * 80f, oldDifferent * 40f);
-            Debug.Log(rotationJet);
+            Vector3 rotationJet = new Vector3(Mathf.Clamp(-100f * vectorMove.x * Mathf.Clamp(waterLineRot + 0.1f,0,1), -40f, 0f), 90f + _oldHorizontal * 80f, oldDifferent * 40f);
+            Debug.Log(newDifferent);
             TransformJet.rotation = Quaternion.Euler(rotationJet);
             yield return new WaitForFixedUpdate();
         }
@@ -56,23 +60,29 @@ public class MovePlayer : MonoBehaviour
         {
             while (waterLineNow > -0.2f)
             {
-                waterLineNow -= waterLineStep*5f;
+                waterLineNow -= waterLineStep * 4f;
+                waterLineRot -= waterLineStep * 5f;
                 yield return new WaitForFixedUpdate();
                 BuoyantObject.waterLevelOffset = waterLineNow;
 
 
             }
-            effect.SetActive(true);
+            Effect.SetActive(true);
             yield return new WaitForFixedUpdate();
 
-            while (waterLineNow < 0.4f)
+            while (waterLineNow < 0.3f)
             {
-                waterLineNow += waterLineStep*1.5f;
-                BuoyantObject.waterLevelOffset = waterLineNow;
+                if (newDifferent < 0.1f)
+                {
+                    waterLineNow += waterLineStep * 1f;
+                    waterLineRot = waterLineNow;
+                    BuoyantObject.waterLevelOffset = waterLineNow;
+                }
                 yield return new WaitForFixedUpdate();
 
             }
-            yield return new WaitForSeconds(Random.Range(0f, 10f) / 10f);
+            _jetDownMove = false;
+            yield return new WaitUntil(() => _jetDownMove);
 
         }
     }
